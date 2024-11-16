badd6c02fc34b670805718d5d73945e35a2b3998
Pragati Gupta <pragatigupta0104@gmail.com>
2024-11-16 14:35:39 +0530

    modules

---
+++ export.js
@@ -0,0 +1,8 @@
+function a(){
+    console.log("a function");
+
+}
+function b(){
+    console.log("b function");
+}
+export{a,b}
---
+++ module.js
@@ -0,0 +1,5 @@
+import sum from './add.js';
+console.log(sum (10,6))
+import {a,b} from './export.js';
+a()
+b()
